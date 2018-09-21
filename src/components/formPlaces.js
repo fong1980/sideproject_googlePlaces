7a86dde9d4082cb508088734a6e4fdf82c532a85
fong1980 <fong1980@gmail.com>
2018-09-21 16:04:07 +0200

    first tempt googlePlaces

--- src/components/formPlaces.js
+++ src/components/formPlaces.js
@@ -1,17 +1,28 @@
 import React, { PureComponent } from "react";
+import Autocomplete from "react-google-autocomplete";
 
 class InputForm extends PureComponent {
   render() {
     return (
       <div>
-        <form name="myForm" onSubmit={this.handleSubmit}>
+        {/* test */}
+        <Autocomplete
+          style={{ width: "90%" }}
+          //   onPlaceSelected={place => {
+          //     console.log(place);
+          //   }
+          // }
+          //types={["(regions)"]}
+          //componentRestrictions={{ country: "ru" }}
+        />
+        {/* <form name="myForm" onSubmit={this.handleSubmit}>
           <input
             name="batchnr"
             placeholder="batchnr"
             value={""}
             onChange={"test"}
           />
-        </form>
+        </form> */}
       </div>
     );
   }
